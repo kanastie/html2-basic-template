95e2e43322ab4ad81e598f45bc5f14d862027883
Anastasia Kolmagorova <canastie@gmail.com>
2024-06-21 16:39:55 +0300

    Стилизует футер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+
+/* Мобильное меню */
+
+const menu = document.querySelector('.header__nav');
+const navToggle = menu.querySelector('.nav__toggle');
+
+navToggle.addEventListener('click', () => {
+  if (menu.classList.contains('nav--closed')) {
+    menu.classList.remove('nav--closed');
+    menu.classList.add('nav--opened');
+  } else {
+    menu.classList.add('nav--closed');
+    menu.classList.remove('nav--opened');
+  }
+});
