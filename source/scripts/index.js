31a7d800508b7635115967646df6490c6e4e720a
Anastasia Kolmagorova <canastie@gmail.com>
2024-06-23 23:37:11 +0300

    Реализует слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -14,3 +14,38 @@ navToggle.addEventListener('click', () => {
     menu.classList.remove('nav--opened');
   }
 });
+
+
+/* Слайдер */
+
+const slider = document.querySelector('.slider');
+const prevButton = slider.querySelector('.slider-button-prev');
+const nextButton = slider.querySelector('.slider-button-next');
+const slides = Array.from(slider.querySelectorAll('.slider__slide'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'flex';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+updateSlider();
